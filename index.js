4bdae5225a130467829c1675822059817fc7affb
Ivan Sosnin <isosnin@skbkontur.ru>
2017-11-10 14:11:00 +0500

    Minor console fix

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const BotMechanics = require('./src/botMechanics.js');
 const bot = new BotMechanics();
 
 process.on('uncaughtException', e => {
+    console.log('***** uncaughtException *****');
     console.log(e);
 });
